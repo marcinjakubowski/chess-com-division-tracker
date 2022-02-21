016fd077825aaf76ad96b2769b452bde02de772a
Marcin Jakubowski <mjakubowski@gmail.com>
2022-02-21 09:01:45 +0100

    fixed relative path to auth.ini

--- code/config.py
+++ code/config.py
@@ -1,11 +1,13 @@
+import os
 from configparser import ConfigParser
 
 DSN = "postgresql+psycopg2://{username}:{password}@{host}:{port}/{db}"
 
 
 def get_config():
+    ini_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'auth.ini'))
     config = ConfigParser()
-    config.read("auth.ini")
+    config.read(ini_path)
     return {
         "dsn": DSN.format_map(dict(config['db'])),
         "sheet_id": config['sheet']['id']
